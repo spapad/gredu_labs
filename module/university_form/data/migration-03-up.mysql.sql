9be8c800ccdbd5e4d6e4488e5432f58e6bbfbf55
Aspasia Katsi <aspasia@aspasia.minedu.gov.gr>
2016-12-09 12:41:53 +0200

    Bug Fixed

--- module/university_form/data/migration-03-up.mysql.sql
+++ module/university_form/data/migration-03-up.mysql.sql
@@ -2,3 +2,5 @@ ALTER TABLE `univ`
 ADD COLUMN `ereunitiko` varchar(191),
 ADD COLUMN `institute` varchar(191),
 ADD COLUMN `other` varchar(191);
+
+ALTER TABLE univ MODIFY COLUMN idrima VARCHAR(191);
